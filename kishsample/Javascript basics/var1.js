51538328c183e7106e0c1037f42f2541d1048cc4
kishb2 <kishb2@gmail.com>
2026-06-26 08:13:27 +0530

    javascript basics

---
+++ kishsample/Javascript basics/var1.js
@@ -0,0 +1,16 @@
+var x=100
+console.log(x)
+
+var b
+b="hello welcome java script"
+console.log(b,x)
+
+let b1=100
+console.log(b1)
+
+console.log("hi kishore");
+
+const bz="kishore"
+bz="kishoreb"
+console.log(bz);
+
